97308f5ec01bab1c930a09648df9c14bfd8f7e3b
JeanMaxSKrebs <jeanmaxskrebs@gmail.com>
2023-04-04 17:50:49 -0300

    inicio

---
+++ src/components/Firebase/ProdutosDao.js
@@ -0,0 +1,70 @@
+import {
+    ref,
+    query,
+    orderByChild,
+    onChildAdded,
+    off,
+    endAt,
+    endBefore,
+    equalTo,
+    startAt,
+    startAfter,
+    onValue,
+    limitToFirst,
+    limitToLast
+} from "firebase/database"
+
+function getOrderByChild(order,db,callback){
+    //Exemplo
+    console.log('getOrderByChild');
+    console.log(order);
+    const refDB = ref(db,'produtos/');
+    const consulta = query(refDB,orderByChild(order))
+    console.log(consulta);
+    onChildAdded(consulta,callback)
+}
+
+function getFilterByChild(filter,value, db,callback){
+    console.log(filter);
+    console.log(value);
+
+    //implement aqui
+    console.log('getFilterByChild');
+    console.log(filter)
+    const refDB = ref(db,'produtos/');
+    const consulta = query(refDB,equalTo(value, value))
+    onChildAdded(consulta,callback)
+}
+
+function getMostExpensive(db,setValue,list){
+    console.log('getMostExpensive');
+
+    // implement aqui
+    /**
+     *     Nesta função é necessário implementar o callback,
+     * pois será necessário ordenar os resultados no cliente
+     * pelos produtos mais caros (reverso). É necessário chamar 
+     * a função setValue() e o array list passados como parametro.
+     * Para repassar os resultados do client React utiliza-se a função
+     * setValue() com os parametros [...list], ou seja, setValue([...list])
+     * onde em list deverá estar o array de produtos ordenados pelo preco
+     * mais caro. Lembrando que list é um array, use os métodos para trabalhar
+     * com arrays em JavaScript! Dica: usem reverse() ou unshift().
+     * 
+     *  */ 
+
+
+}
+
+function getMostCheap(db,callback){
+    console.log('getMostCheap');
+    //implemente aqui
+}
+
+function getPriceRange(value, db,callback){//0--->limit
+    console.log('getPriceRange');
+
+    //implemente aqui
+}
+
+export {getOrderByChild, getFilterByChild, getMostExpensive, getMostCheap, getPriceRange}
